f20e3625b32aa2162a650d77f27755c3ac847053
Neal Platt <neal.platt@gmail.com>
2015-07-11 16:16:05 -0500

    adding text to print SAM headers

--- properCIGAR.pl
+++ properCIGAR.pl
@@ -3,7 +3,7 @@ while(<>){
     ($QNAME,    $FLAG,  $RNAME,     $POS,   $MAPQ,  $CIGAR, 
     $RNEXT,     $PNEXT, $TLEN,      $SEQ,   $QUAL) = split(/\t/);
 
-    if ($CIGAR =~ /^[1|2|3]S\d+M$/ || $CIGAR =~ /^\d+M[1|2|3]S$/ || $CIGAR =~ /^\d+M$/ ){
+    if ($CIGAR =~ /^[1|2|3]S\d+M$/ || $CIGAR =~ /^\d+M[1|2|3]S$/ || $CIGAR =~ /^\d+M$/ || $_ =~ /^@/){
         print "$_\n";
     }
 }
